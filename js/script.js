445b5442760a9b206fd5852647c7fec3b087c62c
Your Name <you@example.com>
2022-07-11 23:58:08 -0300

    newUpdate

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
-let menu = document.querySelector('#menu');
-let menu_bar = document.querySelector('#menu-bar');
+let menu = document.querySelector('#menus');
+let menu_bar = document.querySelector('#menu-bars');
     
   menu_bar.addEventListener('click', function() {
     menu.classList.toggle("menu-toggle");
